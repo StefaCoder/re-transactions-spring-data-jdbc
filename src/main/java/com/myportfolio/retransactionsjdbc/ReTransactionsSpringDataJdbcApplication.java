fa30a733482fb1f9fe018065b9dc9c6164f46e34
StefaCoder <stefa1104@gmail.com>
2024-05-01 09:59:07 -0400

    Initial Commit

---
+++ src/main/java/com/myportfolio/retransactionsjdbc/ReTransactionsSpringDataJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.myportfolio.retransactionsjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReTransactionsSpringDataJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReTransactionsSpringDataJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myportfolio/retransactionsjdbc/ReTransactionsSpringDataJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myportfolio.retransactionsjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReTransactionsSpringDataJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
